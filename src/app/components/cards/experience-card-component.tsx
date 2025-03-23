9be39b3af3dcb210d7f6f8ff10b06c9ac42786f1
Lautaro Olivera <oliveralautaro99@gmail.com>
2025-03-23 17:18:41 -0300

    Ajuste en estilos de experiencia

--- src/app/components/cards/experience-card-component.tsx
+++ src/app/components/cards/experience-card-component.tsx
@@ -1,47 +1,58 @@
 
+import { ActionButtonProps } from "@/app/models/button-custom-model";
 import { Experience } from "@/app/models/experience-model";
+import ActionButtonComponent from "../buttons/action-button-component";
 
 export default function ExperienceCardComponent({ experience }: { experience: Experience }) {
 
-    const experienceParagraphs = experience.responsabilities.map((experience, index) =>
+    const paragraphs = experience.responsabilities.map((experience, index) =>
         <p key={index}>
             {experience}
         </p>
     );
 
+    const props: ActionButtonProps = {
+        texts: [experience.website ? experience.website : ''],
+        icon: {
+            path: "M16.0037 9.41421L7.39712 18.0208L5.98291 16.6066L14.5895 8H7.00373V6H18.0037V17H16.0037V9.41421Z"
+        }
+    }
+
     return (
         <div>
-            <h4>{experience.company}</h4>
-            <div className="experience-subtitle-container">
-                <p>{experience.subtitle}</p>
-                <p>{experience.time}</p>
-            </div>
-            <div className="paragraphs-container">
-                {experienceParagraphs}
-            </div>
-            <div className="experience-data-container">
-                <div className="experience-row">
-                    <p>Position</p>
-                    <p className="bold">{experience.position}</p>
+            <div className="experience-grid">
+                <div className="experience-grid-title">
+                    <h4>{experience.company}</h4>
                 </div>
-                <div className="experience-row">
-                    <p>Location</p>
-                    <p className="bold">{experience.location}</p>
+                <div className="experience-grid-subtitle mar-top-1-5">
+                    <p>{experience.subtitle}</p>
+                    <p>{experience.time}</p>
                 </div>
-                <div className="experience-row">
-                    <p>Industry</p>
-                    <p className="bold">{experience.industry}</p>
+                <div className="experience-grid-text mar-top-1-5">
+                    <section className="paragraphs-container">
+                        {paragraphs}
+
+                    </section>
                 </div>
-                {
-                    experience.website &&
-                    <div className="experience-row">
-                        <p>Website</p>
-                        <a href={experience.website} className="bold button-link link-w-icon">
-                            {experience.website}
-                            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="remixicon css-1tetme1"><path d="M16.0037 9.41421L7.39712 18.0208L5.98291 16.6066L14.5895 8H7.00373V6H18.0037V17H16.0037V9.41421Z"></path></svg>
-                        </a>
+                <div className="experience-grid-table mar-top-1-5">
+                    <div className="experience-table">
+                        <p>Position</p>
+                        <p>{experience.position}</p>
+                        <p>Location</p>
+                        <p>{experience.location}</p>
+                        <p>Industry</p>
+                        <p>{experience.industry}</p>
+                        {
+                            experience.website && <p>Website</p>
+                        }
+                        {
+                            experience.website &&
+                            <a href={experience.website}>
+                                <ActionButtonComponent {...props} />
+                            </a>
+                        }
                     </div>
-                }
+                </div>
             </div>
         </div>
     );
